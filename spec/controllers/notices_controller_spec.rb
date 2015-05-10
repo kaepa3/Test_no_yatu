1f81beb2a87b09ade7241c8a6274fa88c79a091f
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-05-10 16:52:28 +0900

    せめて正常系試験

--- spec/controllers/notices_controller_spec.rb
+++ spec/controllers/notices_controller_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe NoticesController, type: :controller do
+  before do
+    #ログイン情報の作成
+    User.create(user_name: "田中", email: "tanaka@test.co.jp", password: "hoge")
+    @user = User.first
+    allow(controller)
+      .to receive(:current_user)
+      .and_return(@user)
+  end
 
   describe "GET #index" do
     it "returns http success" do
@@ -12,7 +20,7 @@ RSpec.describe NoticesController, type: :controller do
   describe "GET #home" do
     it "returns http success" do
       get :home
-      expect(response).to have_http_status(302)
+      expect(response).to have_http_status(:success)
     end
   end
 end
--- spec/views/notices/home.html.slim_spec.rb
+++ spec/views/notices/home.html.slim_spec.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe "notices/home.html.slim", type: :view do
   before do
   end
-
   it "Display It" do
     @task_inofomations =[]
     render
     expect(rendered).to match /btn-default/
   end
-
 end
